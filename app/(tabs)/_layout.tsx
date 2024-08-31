dcd5c4a01acba9e7b560b3e62e10f6613d8b78b0
SteveGrey <greysteve1980@gmail.com>
2024-08-31 15:34:32 +0100

    init commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,37 +1,60 @@
-import { Tabs } from 'expo-router';
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react';
+import { Tabs } from 'expo-router';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import Colors from '@/constants/Colors';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const Page = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
+    <Tabs screenOptions={{
+        tabBarStyle: {
+            backgroundColor: Colors.bgColor,
+            borderTopWidth: 0,
+            padding: 0
+        },
+
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: Colors.black,
+        tabBarInactiveTintColor: '#999'
+    }}>
+
+        {/* Home */}
+        <Tabs.Screen name='index' options={{
           title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="compass" color={color} />,
+        }}/>
+
+        {/* Category */}
+        <Tabs.Screen name='category' options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="space-dashboard" color={color} />,
+        }} />
+        {/* Search */}
+        <Tabs.Screen name='search' options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => (
+            <View style={{
+                backgroundColor: Colors.primaryColor, 
+                paddingHorizontal: 16, 
+                paddingVertical: 12, 
+                borderRadius: 10,
+                height: 50,
+                }}>
+                <Ionicons size={24} name="search-outline" color={Colors.white} />
+            </View> 
+          )
+        }}/>
+        <Tabs.Screen name='bookmarks' options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="bookmark" color={color} />,
+        }}/>
+        <Tabs.Screen name='profile' options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
+        }}/>
     </Tabs>
-  );
+  )
 }
+
+export default Page
---
+++ data/categories.ts
@@ -0,0 +1,25 @@
+const destinationCategories = [
+    {
+        title: 'All', 
+        iconName: "hiking"
+    },
+    {
+        title: "Beaches",
+        iconName: "beach",
+    },
+    {
+        title: "Mountains",
+        iconName: "terrain",
+    },
+    {
+        title: "Cities",
+        iconName: "city",
+    },
+    {
+        title: "Forest",
+        iconName: "tree",
+    },
+   
+]
+
+export default destinationCategories
